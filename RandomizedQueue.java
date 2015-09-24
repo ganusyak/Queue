ac13c936d2ed5dfb52637c84a3406f50a6b89e91
yuriy ganusyak <y.ganusyak@gmail.com>
2015-09-24 15:56:11 +0300

    Array implementation

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -7,105 +7,67 @@ import edu.princeton.cs.algs4.StdRandom;
  * Created by yuriyganusyak on 9/23/15.
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
-    private Node first;
-    private Node last;
-    private int sizeOfQueue;
-    private int anotherInt;
-
-    private class Node {
-        private Item item;
-        private Node next;
-        private Node previous;
-    }
+    private Item[] array;
+    int sizeOfStack;
 
     public RandomizedQueue() {
-        // empty queue
-        first = null;
-        last = null;
-        sizeOfQueue = 0;
-    }                         // construct an empty deque
-
-    public int size() {
-        return sizeOfQueue;
-    }                        // return the number of items on the deque
+        array = (Item[])new Object[1];
+        sizeOfStack = 0;
+    }
 
     public boolean isEmpty() {
-        return sizeOfQueue == 0;
+        return sizeOfStack == 0;
     }
 
-    private Node nodeWithNumber(int number) {
-        Node currentNode = first;
-        int counter = 0;
-        while (counter < number) {
-            currentNode = currentNode.next;
-            counter++;
+    public int arraySize() {
+        return array.length;
+    }
+
+    public int getSizeOfStack() {
+        return sizeOfStack;
+    }
+
+    public Item itemNumber(int number) {
+        return array[number];
+    }
+
+    private void resizeArrayTo(int size) {
+        Item[] newArray = (Item[]) new Object[size];
+        for (int i = 0; i < sizeOfStack; i++){
+            newArray[i] = array[i];
         }
-        return currentNode;
+        array = newArray;
+        newArray = null;
     }
-
-    public void enqueue(Item item) {      // add new node to the end of queue
-        if (item == null) throw new java.lang.NullPointerException(); // if trying to add null Item
-
-        Node newLast = new Node();
-        newLast.item = item;
-        if (isEmpty()) {
-            first = newLast;
-        } else {
-            newLast.previous = last;
-            last.next = newLast;
+    public void enqueue(Item item) {
+        if (sizeOfStack == array.length) {
+            resizeArrayTo(array.length * 2);
         }
-        last = newLast;
-
-        sizeOfQueue++;                     // increment size of queue counter
-
+        array[sizeOfStack] = item;
+        sizeOfStack++;
     }
 
+    public Item dequeue() {
+        if (sizeOfStack == 0) throw new java.util.NoSuchElementException();
+        int randomIndex = StdRandom.uniform(0, sizeOfStack);
 
+        sizeOfStack--;
+        Item result = array[randomIndex];
+        array[randomIndex] = array[sizeOfStack];
 
-    private Node randomNode() {
-        int numberOfNodeToRemove = StdRandom.uniform(0, sizeOfQueue);
-        return nodeWithNumber(numberOfNodeToRemove);
-    }
-
-    public Item dequeue() {              // remove and return the item from the front
-        if (isEmpty()) throw new java.util.NoSuchElementException();
-
-        Node nodeToRemove = randomNode();
-
-        Item result = nodeToRemove.item;
-        if ((nodeToRemove.next == null) && (nodeToRemove.previous == null)) {
-            // only one element exists
-            first = null;
-            last = null;
-        } else if (nodeToRemove.next == null) {
-            // last element in queue
-            Node newLast = last.previous;
-            newLast.next = null;
-            last = newLast;
-        } else if (nodeToRemove.previous == null) {
-            // first element in queue
-            Node newFirst = first.next;
-            newFirst.previous = null;
-            first = newFirst;
-        } else {
-            Node prevNode = nodeToRemove.previous;
-            Node nextNode = nodeToRemove.next;
-            prevNode.next = nextNode;
-            nextNode.previous = prevNode;
+        if (sizeOfStack <= (array.length / 4)) {
+            resizeArrayTo(array.length / 2);
         }
-        nodeToRemove = null;
-        sizeOfQueue--;
+
+        array[sizeOfStack] = null;
         return result;
-    }                // remove and return the item from the end
+
+    }
 
     public Item sample() {
-        if (isEmpty()) throw new java.util.NoSuchElementException();
-        Node randomNode = randomNode();
-        return randomNode.item;
+        int randomIndex = StdRandom.uniform(0, sizeOfStack);
+        return array[randomIndex];
     }
-
-
     public Iterator<Item> iterator() {
         return new ListIterator();
     }
@@ -116,41 +78,24 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         private Item [] itemArray;
         private int counter = 0;
 
-        private void createOrderArray() {
-            //iterationOrder = new int[sizeOfQueue];
-            itemArray = (Item[]) new Object[sizeOfQueue];
-            Node currentNode = first;
-            for (int i = 0; i < sizeOfQueue; i++) {
-                //iterationOrder[i] = i;
 
-                itemArray[i] = currentNode.item;
-                currentNode = currentNode.next;
-            }
-            StdRandom.shuffle(itemArray);
-            //return iterationOrder;
-        }
 
         @Override
         public Item next() {
-            if ((counter == sizeOfQueue) || (isEmpty())) {
+            if (isEmpty() || counter == sizeOfStack) {
                 throw new NoSuchElementException();
             }
-            if (counter == 0) {
-                createOrderArray();
-            }
+            if (counter == 0) { StdRandom.shuffle(array, 0, sizeOfStack - 1); };
+            Item result = array[counter];
 
-            if (isEmpty()) throw new NoSuchElementException();
-            //Node randomNode = nodeNumber(iterationOrder[counter]);
-            Item result = itemArray[counter];
             counter++;
-            if (counter == sizeOfQueue) itemArray = null;
             return result;
             //return randomNode.item;
         }
 
         @Override
         public boolean hasNext() {
-            return ((counter < sizeOfQueue) && (!isEmpty()));
+            return counter < sizeOfStack;
         }
 
         @Override
@@ -159,8 +104,5 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
 
     }
-    // return an iterator over items in order from front to end
-    public static void main(String[] args) {
-        // here will be unit testing
-    }    // unit testing
+
 }
