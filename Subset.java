d2c4897a2b9fb197733eac45a503d7a777fe6f24
yuriy ganusyak <y.ganusyak@gmail.com>
2015-09-24 19:05:43 +0300

    Final assignment

--- Subset.java
+++ Subset.java
@@ -5,17 +5,7 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class Subset {
     public static void main(String[] args) {
-        int k = Integer.parseInt(args[0]);
-
-        RandomizedQueue queue = new RandomizedQueue();
-        while (!StdIn.isEmpty()) {
-            String string = StdIn.readString();
-            queue.enqueue(string);
-        }
-
-        for (int i = 0; i < k; i++) {
-            StdOut.println(queue.dequeue());
-        }
+        
 
     }
 }
